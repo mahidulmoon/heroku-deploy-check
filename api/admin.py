850ff4696875a50ef759db673e017869e9f5d700
Mahidul Moon <49182474+mahidulmoon@users.noreply.github.com>
2021-12-04 21:18:43 +0600

    models added

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django.contrib.admin.decorators import display
+
+from api.models import Experience, Skills
 
 # Register your models here.
+
+class skillModel(admin.ModelAdmin):
+    list_display = ('id','name','experience_lvl')
+
+admin.site.register(Skills,skillModel)
+admin.site.register(Experience)
--- api/models.py
+++ api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Skills(models.Model): 
+    name = models.CharField(max_length=100,blank=True,null=True)
+    experience_lvl = models.CharField(max_length=50,null=True,blank=True)
+
+
+
+class Experience(models.Model):
+    designation = models.TextField(blank=True,null=True)
+    technology = models.TextField(blank=True,null=True)
+    company = models.TextField(blank=True,null=True)
+    startDate = models.TextField(blank=True,null=True)
+    endDate = models.TextField(blank=True,null=True)
